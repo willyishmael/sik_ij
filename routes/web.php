099bae72503ae5a4d1ffffe397c455b8240569ec
willyishmael <wtangka22@gmail.com>
2021-09-05 18:13:29 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::get('/login', [LoginController::class, 'index'])->name('login');//->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 
 Route::get('/dashboard', [DashboardController::class, 'index']);//->middleware('auth');
-=======
-Route::get('/login', [LoginController::class, 'index'])->name('login'); //->middleware('guest');
-Route::post('/login', [LoginController::class, 'authenticate']);
-
-Route::get('/dashboard', [DashboardController::class, 'index']); //->middleware('auth');
->>>>>>> ee86940ec08fb673ea4dfd66123705d4975f3015
