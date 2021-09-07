0a7cd0416edec7e5422d5c6b9b77db6ba5a84620
jonathanphandany <jonathanphandany@gmail.com>
2021-09-07 11:35:20 +0800

    tiba" bisa jadi karena ui

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,23 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SuperAdminController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');//->middleware('guest');
-Route::post('/login', [LoginController::class, 'authenticate']);
+// Route::get('/login', [LoginController::class, 'index'])->name('login');//->middleware('guest');
+// Route::post('/login', [LoginController::class, 'authenticate']);
 
-Route::get('/dashboard', [DashboardController::class, 'index']);//->middleware('auth');
+// Route::get('/dashboard', [DashboardController::class, 'index']);//->middleware('auth');
 
-// Route::get('/admin', 'AdminController@index');
-// Route::get('/superadmin', 'SuperAdminController@index');
+// // Route::get('/admin', 'AdminController@index');
+// // Route::get('/superadmin', 'SuperAdminController@index');
 
-Route::get('/admin', [AdminController::class, 'index']);
-Route::get('/superadmin', [SuperAdminController::class, 'index']);
+// Route::get('/admin', [AdminController::class, 'index']);
+// Route::get('/superadmin', [SuperAdminController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
