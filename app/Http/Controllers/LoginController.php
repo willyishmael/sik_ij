eb9587e4478d426c769c7acf29a6cbda0bf6ea48
jonathanphandany <jonathanphandany@gmail.com>
2021-09-11 19:51:07 +0800

    return json

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
@@ -26,11 +27,19 @@ class LoginController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('/dashboard');
+            // return redirect()->intended('/dashboard');
+            return response()->json([
+                'email' => $credentials['email'],
+                'password' => $credentials['password'],
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'salah no nn pokoknya'
+            ], 401);
         }
 
-        return back()->with('loginError', 'Login failed!');
+        // return back()->with('loginError', 'Login failed!');
     }
 
-    // tesssss
+    
 }
